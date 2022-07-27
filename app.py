1149702917fb27e2fd4da05a12419c3822138e3b
wang-jun@bmsoft.com.cn <wangjun@681>
2022-07-27 17:41:54 +0800

    test

--- app.py
+++ app.py
@@ -1,19 +1,39 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/admin/<name>')
-def hello_world(name):
-    return '显示路由：{}'.format(name)
+# @app.route('/admin')
+# def hello_admin():
+#     return 'hello admin'
+#
+# @app.route('/guest/<guest>')
+# def hello_guest(guest):
+#     return f'hello{guest}'
+#
+# @app.route('/user/<name>')
+# def hello_user(name):
+#     if name == 'admin':  # url_for函数构造特定的URL
+#         return redirect(url_for('hello_admin'))
+#     else:
+#         return redirect(url_for('hello_guest',guest=name))
 
-@app.route('/blog/<int:postID>')
-def show_blog(postID):
-    return f'编号{postID}'
+@app.route('/')
+def index():
+    return render_template('login.html')
 
-@app.route('/rev/<float:revNo>')
-def revsion(revNo):
-    return f'版本号{revNo}'
+@app.route('/success/<name>')
+def success(name):
+    return f'welcome {name}'
+
+@app.route('/login',methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5555, debug=True)
